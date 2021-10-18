cd33b23c4b473d39cd14097b4a704498ae3077c5
0905022338 <91861293+0905022338@users.noreply.github.com>
2021-10-18 11:30:12 +0800

    Add files via upload

---
+++ Caaa.java
@@ -0,0 +1,15 @@
+
+public class Caaa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 3;
+		int c = a*b;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+	}
+
+}
